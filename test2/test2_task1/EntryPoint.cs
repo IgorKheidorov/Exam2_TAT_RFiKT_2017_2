364d2d6ab0dff91254f0b6d7844108676bfe3759
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-11-21 22:27:57 +0300

    Add test2 task1

---
+++ test2/test2_task1/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test2_task1
+{
+    class EntryPoint
+    {
+        private const string MESSAGE = "YOUR DATA IS NOT CORRECT. TRY AGAIN:!";
+        private const string INPUTDATA = "Input your string:";
+
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+            while (continueProgram)
+            {
+                Console.WriteLine(INPUTDATA);
+                string inputString = Console.ReadLine();
+                char[] splitString = inputString.ToCharArray();
+                StringWorker stringWorker = new StringWorker();
+                if(stringWorker.IsValid(splitString))
+                {
+                   int result =  stringWorker.ConvertToInt(splitString);
+                }
+            }
+        }
+    }
+}
---
+++ test2/test2_task1/StringWorker.cs
@@ -0,0 +1,71 @@
+﻿namespace test2_task1
+{
+    public class StringWorker
+    {
+        public bool IsValid(char[] splitString)
+        {
+            return IsLengthValid(splitString) && IsSymbolsValid(splitString);
+        }
+
+        private bool IsLengthValid(char[] splitString)
+        {
+            bool validation = false;
+            if(splitString.Length > 0 || (splitString[0] == '-' && splitString.Length > 1))
+            {
+                validation = true;
+            }
+            return validation;
+        }
+
+        private bool IsNegative(char[] splitString)
+        {
+            bool negative = false;
+            if (splitString[0] == '-')
+            {
+                negative = true;
+            }
+            return negative;
+        }
+
+        private bool IsSymbolsValid(char[] splitString)
+        {
+            bool validation = true;
+            for(int i = 0; i < splitString.Length; i++)
+            {
+                if (i == 0 && splitString[i] == '-')
+                {
+                    continue;
+                }
+                if (splitString[i] == '.' || splitString[i] == ',')
+                {
+                    
+                }
+                if (splitString[i] < '0' || splitString[i] > '9')
+                {
+                   return validation = false;
+                }
+                
+            }
+            return validation;
+        }
+
+        public int ConvertToInt(char[] splitString)
+        {
+            int result = 0;
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                if (i == 0 && splitString[i] == '-')
+                {
+                    continue;
+                }
+                if (splitString[i] == '.' || splitString[i] == ',')
+                {
+                    continue;
+                }
+                result = result * 10 + (splitString[i] - '0');
+            }
+            return result;
+        }
+
+    }
+}
