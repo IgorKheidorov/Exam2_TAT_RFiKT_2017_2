5dbbc1f4c136b8b0e1884b2f51f4a8e559164c23
MorozAnn <amoroz408@gmail.com>
2017-11-21 22:01:21 +0300

    added task 1

---
+++ StringToIntegerParser/StringToIntegerParser/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToIntegerParser
+{
+  class EntryPoint
+  {
+    static void Main()
+    {
+      Console.WriteLine("Enter string");
+      string enteredString = Console.ReadLine();
+      StringContainsManipulator stringContainsManipulator = new StringContainsManipulator();
+      bool Check = stringContainsManipulator.IntChecker(enteredString);
+      if (Check == true)
+      {
+        Console.WriteLine("Number =" + stringContainsManipulator.StringParserToInt(enteredString));
+      }
+      else
+      {
+        Console.WriteLine("This string can not be converted");
+      }
+      Console.ReadKey();
+    }
+  }
+}
---
+++ StringToIntegerParser/StringToIntegerParser/StringContainsManipulator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToIntegerParser
+{
+  class StringContainsManipulator
+  {
+    const char ZERO = '0';
+    const char NINE = '9';
+    const string MINUS = "-";
+
+    /// <summary>
+    /// This method checks whether all parts of the string are numbers  
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
+    public bool IntChecker(string line)
+    {
+      bool checker = true;
+      for (int i = 0; i < line.Length; i++)
+      {
+        if (line[i] < ZERO || line[i] > NINE)
+        {
+          return false;
+        }
+      }
+      return checker;
+    }
+
+    private bool CheckForNegativity(string line)
+    {
+      return line.StartsWith(MINUS);
+    }
+    private static int CharToInt(char c)
+    {
+      return c - ZERO;
+    }
+
+    /// <summary>
+    /// This method parse string to int
+    /// </summary>
+    /// <returns></returns>
+    public int StringParserToInt(string line)
+    {
+      int finiteNumber = 0;
+      for (int i = 0, j = 1; i < line.Length; i++, j++)
+      {
+        finiteNumber += CharToInt(line[i]) * (int)Math.Pow(10, line.Length - j);
+        if (CheckForNegativity(line))
+        {
+          finiteNumber=finiteNumber * (-1);
+        }
+        if (finiteNumber > int.MaxValue || finiteNumber < int.MinValue)
+        {
+          throw new StackOverflowException();
+        }
+      }
+      return finiteNumber;
+    }
+  }
+}
