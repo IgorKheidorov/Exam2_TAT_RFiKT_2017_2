75bf329600f5ef20108194c960770c54ebedb029
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-21 22:56:04 +0300

    Tests.

---
+++ Exam/TestTask1/IntegerParserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exam;
+
+namespace TestTask1
+{
+    [TestClass]
+    public class IntegerParserTest
+    {
+        [TestMethod]
+        public void TestPositiveNumber()
+        {
+            Assert.AreEqual(123, new IntegerParser("123").Parse());
+        }
+        [TestMethod]
+        public void TestNegativeNumber()
+        {
+            Assert.AreEqual(-12, new IntegerParser("-12").Parse());
+        }
+
+    }
+}
