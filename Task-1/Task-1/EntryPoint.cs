4c38ce8d331263f458513b5ef86d959c31101262
Vadim Belkin <vortorent@mail.ru>
2017-11-21 22:46:36 +0300

    Task without tests

---
+++ Task-1/Task-1/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.WriteLine("Enter string: ");
+        StringToIntConverter converter = new StringToIntConverter();
+        Console.WriteLine(converter.ConvertStringToInt(Console.ReadLine()));
+      }
+      catch(Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+  }
+}
---
+++ Task-1/Task-1/StringToIntConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_1
+{
+  /// <summary>
+  /// This class converts string to int if it can.
+  /// </summary>
+  class StringToIntConverter
+  {
+    private const char FirstIntNumber = '0';
+    private const char LastIntNumber = '9';
+    private const char Minus = '-';
+
+    private int ConvertSymbolToInt(char symbol)
+    {
+      return symbol - FirstIntNumber;
+    }
+
+    /// <summary>
+    /// Converts string to int if it can.
+    /// </summary>
+    /// <param name="text">String, which need to convert to int.</param>
+    /// <returns>Converted string.</returns>
+    public int ConvertStringToInt(string text)
+    {
+      if (text.Length == 0)
+      {
+        throw new ArgumentException();
+      }
+
+      int firstSymbolIsMinus = 0;
+      if (text[0] == Minus && text.Length == 1)
+      {
+        throw new ArgumentException();
+      }
+      else if (text[0] == Minus && text.Length > 1)
+      {
+        firstSymbolIsMinus++;
+      }
+
+      int resultNumber = 0;
+      for (int i = firstSymbolIsMinus; i < text.Length; i++)
+      {
+        if (text[i] < FirstIntNumber || text[i] > LastIntNumber)
+        {
+          throw new ArgumentException();
+        }
+        else
+        {
+          resultNumber = (resultNumber * 10 + ConvertSymbolToInt(text[i]));
+        }
+      }
+      if(firstSymbolIsMinus == 1)
+      {
+        resultNumber = 0 - resultNumber;
+      }
+      return resultNumber;
+    }
+  }
+}
