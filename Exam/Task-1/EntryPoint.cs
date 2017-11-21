2e5e77422d8e01c3db102a0aee01357d6e512649
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-21 22:34:13 +0300

    First task.

---
+++ Exam/Task-1/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exam
+{
+    /// <summary>
+    /// Entry point to the program.
+    /// Program that checks if inputed string can present an integer value,
+    /// If yes converts it to Iteger value and output.
+    /// </summary>
+    class EntryPoint
+    {
+        public const string INPUTPROMPT = "Input value: ";
+        /// <summary>
+        /// Main class of the program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(INPUTPROMPT);
+                string inputValue = Console.ReadLine();
+                Console.WriteLine(new IntegerParser(inputValue).Parse());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Exam/Task-1/IntegerParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exam
+{
+    /// <summary>
+    /// Class for parsing inputed string.
+    /// </summary>
+    class IntegerParser
+    {
+        public const int ASCINORMALISATION = 48;
+        public const char MINUS = '-';
+        public const char ONE = '1';
+        public const char NINE = '9';
+        public const string COMMA = ",";
+        public char[] InputedValue { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="inpValue"> User inputed value</param>
+        public IntegerParser(string inpValue)
+        {
+            if (inpValue.Contains(COMMA))
+            {
+                inpValue = inpValue.Remove(inpValue.IndexOf(COMMA));
+                InputedValue = inpValue.ToCharArray();
+            }
+            else
+            {
+                InputedValue = inpValue.ToCharArray();
+            }
+        }
+        /// <summary>
+        /// Parses string to int
+        /// </summary>
+        /// <returns> Parsed string </returns>
+        public int Parse()
+        {
+            bool isNegative = false;
+            int resultValue = 0;
+            int startPosition = 0;
+            if (InputedValue[0] == MINUS && InputedValue.Length > 0)
+            {
+                isNegative = true;
+                startPosition = 1;
+            }
+            for ( int i = startPosition; i < InputedValue.Length; i++ )
+            {
+                if (InputedValue[i] >= ONE && InputedValue[i] <= NINE)
+                {
+                    resultValue += ( InputedValue[i] - ASCINORMALISATION ) 
+                        *  (int)Math.Pow(10, InputedValue.Length - i - 1);
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+            if ( resultValue > int.MaxValue)
+            {
+                throw new FormatException();
+            }
+            if ( isNegative )
+            {
+                resultValue = - resultValue;
+            }
+            return resultValue;
+        }
+    }
+}
