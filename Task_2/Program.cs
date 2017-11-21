71b65ddb1f8d9447f54d08a3758a2455a6312433
ButurlyaAL <stdlibh1421@gmail.com>
2017-11-21 21:59:34 +0200

    Task_2 without tests

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Task_2
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringAnalyzer analyzer = new StringAnalyzer("dfgr");
+            Console.WriteLine(analyzer.StringCanBeCreatingFrom("fgrd"));
+        }
+    }
+}
---
+++ Task_2/StringAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Analyzes string.
+    /// </summary>
+    class StringAnalyzer
+    {
+        private string stringToAnalyze;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="stringToAnalyze">String to analyze.</param>
+        public StringAnalyzer(string stringToAnalyze)
+        {
+            this.stringToAnalyze = stringToAnalyze;
+        }
+
+        /// <summary>
+        /// Check if string can be creating from other.
+        /// </summary>
+        /// <param name="otherString">Other string</param>
+        /// <returns>True if string can be creating from other; otherwise false.</returns>
+        public bool StringCanBeCreatingFrom(string otherString)
+        {
+            foreach (char symbol in otherString)
+            {
+                if (!stringToAnalyze.Contains(symbol))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
