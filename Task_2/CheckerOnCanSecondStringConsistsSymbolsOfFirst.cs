9510d6250e71615bcfc20cbeedb29e99be81e82a
nikitoshom <nikitos_hom@mail.ru>
2017-11-21 23:00:00 +0300

    Task_2_StringFromString

---
+++ Task_2/CheckerOnCanSecondStringConsistsSymbolsOfFirst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanSecondStringConsistsFromFirstString
+{
+  public class CheckerOnCanSecondStringConsistsSymbolsOfFirst
+  {
+    String firstString;
+    String secondString;
+
+    public CheckerOnCanSecondStringConsistsSymbolsOfFirst(String firstString, String secondString)
+    {
+      this.firstString = firstString;
+      this.secondString = secondString;
+    }
+
+    public Boolean CheckOnSecondStringHasAllSymbolsOfFirst()
+    {
+      int counter = 0;
+      for(int i = 0; i <= secondString.Length; i++)
+      {
+        if (firstString.Contains(secondString[i]))
+        {
+          counter = counter + 1;
+        }
+      }
+      if (counter == secondString.Length)
+      {
+        return true;
+      }
+      return false;
+    }
+  }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CanSecondStringConsistsFromFirstString
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        CheckerOnCanSecondStringConsistsSymbolsOfFirst checkerOnCanSecondStringConsistsSymbolsOfFirst = new CheckerOnCanSecondStringConsistsSymbolsOfFirst(args[0], args[1]);
+        Console.WriteLine(checkerOnCanSecondStringConsistsSymbolsOfFirst.CheckOnSecondStringHasAllSymbolsOfFirst());
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine(exc.Message);
+      }
+    }
+  }
+}
