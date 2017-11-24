e6c8de9b3a7feeb98186f88a78736e8f60da9bd6
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-24 21:52:27 +0300

    added projects

--- Tasks/Task2/EntryPoint.cs
+++ Tasks/Task2/EntryPoint.cs
@@ -1,27 +1,24 @@
 ﻿using System;
-using System.Linq;
 
 namespace Task2
 {
+  /// <summary>
+  /// 
+  /// </summary>
   public class EntryPoint
   {
+    private const string Possible = "Possible";
     private const string Inpossible = "Inpossible";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
-      string line1 = "abcdef";
-      string line2 = "fbcdef";
-
-      foreach (var item1 in line1)
-      {
-        if (line2.Contains(item1))
-        {
-          line2 = line2.Remove(line2.IndexOf(item1), 0);
-        }
-        else
-        {
-          Console.WriteLine(Inpossible);
-        }
-      }
+      string result = new StringsAnalyzer()
+        .IsPossibleCreateFirstStringFromElemsFromSecond("abcdef1", "fedcba") ? Possible : Inpossible;
+      Console.WriteLine(result);
     }
   }
 }
---
+++ Tasks/Task2/StringsAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Task2
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class StringsAnalyzer
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="firstString"></param>
+    /// <param name="secondString"></param>
+    /// <returns></returns>
+    public bool IsPossibleCreateFirstStringFromElemsFromSecond(string firstString, string secondString)
+    {
+      string line1 = firstString;
+      string line2 = secondString;
+      bool isPossible = false;
+
+      foreach (var item1 in line1)
+      {
+        if (line2.Contains(item1))
+        {
+          line2 = line2.Remove(line2.IndexOf(item1), 0);
+          isPossible = true;
+        }
+        else
+        {
+          isPossible = false;
+          break;
+        }
+      }
+
+      return isPossible;
+    }
+  }
+}
