3ae28c91f966a659ad43e0833e1421e03edfba37
KlyuchevenkoEkaterina <ktrin1996@mail.ru>
2017-11-24 21:59:23 +0200

    Add task

---
+++ TASK_XML/DOM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace DOM
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string path = @"C:\Users\Kate\Desktop\Exs.txt";
+      string line;
+      using (StreamReader sr = new StreamReader(path))
+      {
+        line = sr.ReadToEnd();
+      }
+      Searcher searcher = new Searcher();
+      searcher.FindSuccessor(line);    
+    }
+  }
+}
---
+++ TASK_XML/DOM/Searcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DOM
+{
+  class Searcher
+  {
+    /// <summary>
+    /// This method is used to search tags in XML document.
+    /// </summary>
+    /// <param name="line">Data from xml document</param>
+    public List<string> GetTags(string line)
+    {
+      Regex regex = new Regex(@"<[^>]+>");
+      MatchCollection matches = regex.Matches(line);
+      List<string> listOftags = new List<string>();
+      foreach (var tag in matches)
+      {
+        listOftags.Add(tag.ToString());
+      }
+      return listOftags;
+    }
+
+    /// <summary>
+    /// Find start and end index of tags.
+    /// </summary>
+    /// <param name="listOftags">Array Of tags</param>
+    /// <param name="line">Data from xml document</param>   
+    public string FindSuccessor(List<string> listOftags, string line)
+    {
+      string inheritor = null;
+      for (int i = 0; i < listOftags.Count; i++)
+      {
+        int firstIndex = line.IndexOf(listOftags[i]);
+        int endIndex = line.IndexOf(listOftags[i][0].ToString() + '\\');
+        inheritor = line.Substring(firstIndex, endIndex);
+      }
+      return inheritor;
+    }
+  }
+}
