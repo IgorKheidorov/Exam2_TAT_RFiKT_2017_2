08b6298f73734dbd5256a9fd3179428f634321d0
KlyuchevenkoEkaterina <ktrin1996@mail.ru>
2017-11-27 21:22:32 +0200

    Add task(

---
+++ TaskWithBrackets/Brackets.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BracketsOfDifferentTypes
+{
+  class Brackets
+  {
+    Stack<char> bracketStack = new Stack<char>();
+
+    public void CheckInputStringForBrackets(string inputString)
+    {     
+      int round = 0;
+      int figured = 0;
+      int squared = 0;          
+      for (int i = 0; i < inputString.Length; i++)
+      {
+        if (inputString[i] == '(') round++;
+        else if (inputString[i] == ')') round--;
+        else if (inputString[i] == '{') figured++;
+        else if (inputString[i] == '}') figured--;
+        else if (inputString[i] == '[') squared++;
+        else if (inputString[i] == ']') squared--;
+
+        CheckExcessBracket(round, figured, squared);
+        AddToStack(inputString);
+
+
+
+
+      }
+    }
+
+    /// <summary>
+    /// Checks if the closing bracket is on the corresponding opening.
+    /// </summary>
+    /// <param name="round">number round brackets</param>
+    /// <param name="figured">number figured brackets</param>
+    /// <param name="squared">number squared brackets</param>
+    public void CheckExcessBracket(int round, int figured, int squared)
+    {
+      if (round < 0 || figured < 0 || squared < 0)
+      {        
+        Console.WriteLine("Excess closing bracket");        
+      }
+    }
+
+    /// <summary>
+    /// Adds the opening brackets to the stack.
+    /// </summary>
+    /// <param name="inputString">yout input string</param>
+    /// <returns>stack with brackets</returns>
+    public Stack<char> AddToStack(string inputString)
+    {
+      for (int i = 0; i < inputString.Length; i++)
+      {
+        if ((inputString[i] == '(') || (inputString[i] == '{') || (inputString[i] == '['))
+        {                   
+          bracketStack.Push(inputString[i]);         
+        }
+      }
+      return bracketStack;
+    }
+  }
+}
---
+++ TaskWithBrackets/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BracketsOfDifferentTypes
+{
+  /// <summary>
+  /// The entry point of the program.
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Input string");
+      string inputString = Console.ReadLine();
+      Brackets brackets = new Brackets();
+      brackets.CheckInputStringForBrackets(inputString);
+    }
+  }
+}
