6c33f8ca1e4ca5acf8498481071a88637c74a933
Evgeny Kisel <kisel.jenya@gmail.com>
2017-11-27 21:32:25 +0300

    added brackets

---
+++ AlgebraicExpression/AlgebraicExpression.Tests/BracketsChackerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgebraicExpression.Tests
+{
+  [TestClass]
+  public class BracketsChackerTests
+  {
+    [TestMethod]
+    public void CheckBrackets_expressionWithBrackets_trueValue()
+    {
+      Assert.AreEqual(true, new Expression("()[]").GetBrackets());
+    }
+
+    [TestMethod]
+    public void CheckBrackets_emptyExpression_trueValue()
+    {
+      Assert.AreEqual(true, new Expression(" ").GetBrackets());
+    }
+  }
+}
