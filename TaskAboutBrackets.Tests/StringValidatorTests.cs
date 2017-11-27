76e7a70edaada6d4ee463373e775545171a2734e
ButurlyaAL <stdlibh1421@gmail.com>
2017-11-27 20:23:57 +0200

    taskaboutbrackets

---
+++ TaskAboutBrackets.Tests/StringValidatorTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskAboutBrackets.Tests
+{
+    [TestClass]
+    public class StringValidatorTests
+    {
+        StringValidator validator = new StringValidator();
+        [TestMethod]
+        public void TestStringIsValidOnValidAlignmentWithEmbeddedBracketsWithAllTypesOfBrackets()
+        {
+            Assert.IsTrue(validator.StringIsValid("{ [ ( ) ] }"));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnValidAlignmentWithBraces()
+        {
+            Assert.IsTrue(validator.StringIsValid("{ }"));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnValidAlignmentWithRoundBrackets()
+        {
+            Assert.IsTrue(validator.StringIsValid("( )"));
+        }
+
+
+        [TestMethod]
+        public void TestStringIsValidOnValidAlignmentWithSquareBrackets()
+        {
+            Assert.IsTrue(validator.StringIsValid("[ ]"));
+        }
+
+
+        [TestMethod]
+        public void TestStringIsValidOnValidConsistentAlignmentWithAllTypesOfBrackets()
+        {
+            Assert.IsTrue(validator.StringIsValid("{ }[ ]( )"));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnInvalidAlignmentWithBracesAndRoundBrackets()
+        {
+            Assert.IsFalse(validator.StringIsValid("{ ( } ) "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnInvalidAlignmentWithBracesAndSquareBrackets()
+        {
+            Assert.IsFalse(validator.StringIsValid("{ [ } ] "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnClosingBracketInTwoRoundBracketsWithRightAlignement()
+        {
+            Assert.IsFalse(validator.StringIsValid(" ( ) ) "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnClosingBracketInTwoSquareBracketsWithRightAlignement()
+        {
+            Assert.IsFalse(validator.StringIsValid(" [ ] ] "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnClosingBracketInTwoBracesWithRightAlignement()
+        {
+            Assert.IsFalse(validator.StringIsValid(" { } } "));
+        }
+
+     
+
+        [TestMethod]
+        public void TestStringIsValidOnOneClosingBrace()
+        {
+            Assert.IsFalse(validator.StringIsValid(" } "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnOneClosingSquareBracket()
+        {
+            Assert.IsFalse(validator.StringIsValid(" ] "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnOneClosingRoundBracket()
+        {
+            Assert.IsFalse(validator.StringIsValid(" ) "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnOneOpeningRoundBracket()
+        {
+            Assert.IsFalse(validator.StringIsValid(" ( "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnOneOpeningSquareBracket()
+        {
+            Assert.IsFalse(validator.StringIsValid(" [ "));
+        }
+
+        [TestMethod]
+        public void TestStringIsValidOnOneOpeningBrace()
+        {
+            Assert.IsFalse(validator.StringIsValid(" { "));
+        }
+    }
+}
---
+++ TaskAboutBrackets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAboutBrackets
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringValidator validator = new StringValidator();
+            Console.WriteLine("Enter an expression :");
+            Console.WriteLine(validator.StringIsValid(Console.ReadLine()));
+        }
+    }
+}
