796cc19d0184ea73d66a4d32ece819479eb585ec
Volodko Julia <julia.volodko.2011@mail.ru>
2017-11-27 18:29:22 +0300

    Delete commits.

--- checkParentheses/checkParentheses/EntryPoint.cs
+++ checkParentheses/checkParentheses/EntryPoint.cs
@@ -7,19 +7,14 @@ namespace CheckParentheses
   /// </summary>
   public class EntryPoint
   {
-    const string INPUTSEQUENCE = "Enter your sequence:\n >>";
-
     /// <summary>
-    /// 
+    /// Call all methods.
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args)
     {
       try
       {
-        // Console.Write(INPUTSEQUENCE);
-        //string stringForChecking = "((()]))";
-        //Console.ReadLine()
         CheckerOfParentheses checker = new CheckerOfParentheses(args[0]);
         Console.WriteLine(checker.CheckerSequence());
       }
