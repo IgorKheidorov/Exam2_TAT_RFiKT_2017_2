190d35f0fc4c4a190979cc3bc581aa04efd1c779
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-11-27 21:18:14 +0300

    Add task

---
+++ BracketsTask/BracketsTask.Test/BracketTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BracketsTask.Test
+{
+    [TestClass]
+    public class BracketTests
+    {
+        [TestMethod]
+        public void IsValidWithCorrectPositive()
+        {
+            Assert.IsTrue(new Brackets().IsValid("([{}])"));
+        }
+
+        [TestMethod]
+        public void IsValidWithAllRoundBracketsPositive()
+        {
+            Assert.IsTrue(new Brackets().IsValid("(())"));
+        }
+
+        [TestMethod]
+        public void IsValidWithNotValidPositionBracketsString()
+        {
+            Assert.IsFalse(new Brackets().IsValid("([{])}"));
+        }
+        [TestMethod]
+        public void IsValidWithStringWithoutBrackets()
+        {
+            Assert.IsFalse(new Brackets().IsValid("111"));
+        }
+    }
+}
---
+++ test2/XmlPArserTask.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XmlPArserTask.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XmlPArserTask.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b06a0ca0-9999-4572-ae50-ff45cc78f114")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
