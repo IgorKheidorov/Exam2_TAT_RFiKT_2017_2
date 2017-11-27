dcbae46a94c1b6f620761e90e0add7468f23db8e
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-11-27 21:28:35 +0300

    update main

--- Test27.11/src/Main.java
+++ Test27.11/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
   public static void main(String[] args) {
     try {
-      new Validator().checkBracketsAlignment("(){}[]");
+      if (new Validator().checkBracketsAlignment("(){}[]")) {
+        System.out.println("The brackets are arranged correctly.");
+      }
     } catch (Exception e) {
       System.out.println("Exception" + e.getMessage());
     }
