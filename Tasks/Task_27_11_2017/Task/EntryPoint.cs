a97b260f1a8f7a70aabf7629a31751985e82ee9f
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-27 21:30:15 +0300

    added main logic

--- Tasks/Task_27_11_2017/Task/EntryPoint.cs
+++ Tasks/Task_27_11_2017/Task/EntryPoint.cs
@@ -1,25 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace Task
 {
   /// <summary>
-  /// Class contains dictionary with possible brackets
+  /// Main method of class calls expression analyzer
+  /// to analyze input in command line exppression
   /// </summary>
   public class EntryPoint
   {
-    public Dictionary<char, char> BracketsDictionary = new Dictionary<char, char>()
-    {
-      {'}','{' },
-      {']','[' },
-      {')','(' }
-    };
+    private const string ValidExpression = "Valid expression";
+    private const string InvlidExpression = "Invalid expression";
+
     /// <summary>
-    /// 
+    /// Method passes the first argument to the command line
+    /// and gives the result of the analysis of the arrangement of the brackets
+    /// described in the dictionary
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args)
     {
-
+      Dictionary<char, char> bracketsDictionary = new Dictionary<char, char>()
+      {
+        {'}','{' },
+        {']','[' },
+        {')','(' }
+      };
+      string result = new AlgebraicExpressionAnalyzer()
+        .IsBracketAlignmentValid(args[0], bracketsDictionary) ? ValidExpression : InvlidExpression;
+      Console.WriteLine(result);
     }
   }
 }
