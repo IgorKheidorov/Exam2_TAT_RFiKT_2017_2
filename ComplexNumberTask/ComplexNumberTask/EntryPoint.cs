47b24d52862c31d8d9f4f3c553dfde7959245016
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-11-28 21:16:58 +0300

    Add ComplexNumberTask

---
+++ ComplexNumberTask/ComplexNumberTask/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ComplexNumberTask
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber complexNumber1 = new ComplexNumber(2, 3);
+            ComplexNumber complexNumber2 = new ComplexNumber(20, 30);
+            complexNumber1.Display();
+
+        }
+    }
+}
---
+++ ComplexNumberTask/ComplexNumberTests/ComplexNumberTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComplexNumberTask;
+
+namespace ComplexNumberTests
+{
+    [TestClass]
+    public class ComplexNumberTests
+    {
+        [TestMethod]
+        public void ToStingWithCorrectIntNumberPositive()
+        {
+            Assert.AreEqual("2 + i * 3", new ComplexNumber(2, 3).ToString());
+        }
+
+        [TestMethod]
+        public void ToStingWithCorrectDoubleNumberPositive()
+        {
+            Assert.AreEqual("2,003 + i * 3,1", new ComplexNumber(2.003, 3.1).ToString());
+        }
+
+        [TestMethod]
+        public void ToStingForCorrectDisplayNumberNegative()
+        {
+            Assert.AreNotEqual("2+ i * 3", new ComplexNumber(2, 3).ToString());
+        }
+
+        [TestMethod]
+        public void GetModulOfComplexNumberWithCorrectDataPositive()
+        {
+            Assert.AreEqual(5, new ComplexNumber(3, 4).GetModulOfComplexNumber());
+        }
+
+        [TestMethod]
+        public void GetModulOfComplexNumberWithCorrectDoubleDataPositive()
+        {
+            Assert.AreEqual(5, new ComplexNumber(3.0, 4.0).GetModulOfComplexNumber());
+        }
+
+        [TestMethod]
+        public void SumOfComplexNumberWithCorrectDataPositive()
+        {
+            Assert.AreEqual(new ComplexNumber(4, 6).ToString(), (new ComplexNumber(3, 4) + new ComplexNumber(1, 2)).ToString());
+        }
+
+        [TestMethod]
+        public void SumOfComplexNumberWithCorrectDoubleDataPositive()
+        {
+            Assert.AreEqual(new ComplexNumber(4.2, 6.2).ToString(), (new ComplexNumber(3.1, 4.1) + new ComplexNumber(1.1, 2.1)).ToString());
+        }
+
+        [TestMethod]
+        public void DifferenceOfComplexNumberWithCorrectDoubleDataPositive()
+        {
+            Assert.AreEqual(new ComplexNumber(2, 2).ToString(), (new ComplexNumber(3.1, 4.1) - new ComplexNumber(1.1, 2.1)).ToString());
+        }
+
+    }
+}
