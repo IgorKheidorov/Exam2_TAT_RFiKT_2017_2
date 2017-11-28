4115c0813fa22ab6e9b8cf506a68f6f6c781eb40
MorozAnn <amoroz408@gmail.com>
2017-11-28 21:31:21 +0300

    Added tests

---
+++ ComplexNumber/ComplexNumber.Tests/ComplexNumberTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComplexNumbers.Tests
+{
+  [TestClass]
+  public class ComplexNumberTests
+  {
+    [TestMethod]
+    public void Compare_twoComplexNumbersWithHigherFirstNamber_possitiveValue()
+    {
+      Assert.AreEqual(1, new ComplexNumber(2, 1).Compare(new ComplexNumber(1, 1)));
+    }
+
+    [TestMethod]
+    public void Sum_twoComplexNumbers_sumValue()
+    {
+      Assert.AreEqual("2+2i", (new ComplexNumber(1, 1) + new ComplexNumber(1, 1)).ToString());
+    }
+
+    [TestMethod]
+    public void Multiplication_twoComplexNumbers_multiplicityValue()
+    {
+      Assert.AreEqual("0+2i", (new ComplexNumber(1, 1) * new ComplexNumber(1, 1)).ToString());
+    }
+
+    [TestMethod]
+    public void Division_twoComplexNumbers_divisionValue()
+    {
+      Assert.AreEqual("1+0i", (new ComplexNumber(1, 1) / new ComplexNumber(1, 1)).ToString());
+    }
+
+    [TestMethod]
+    public void DIfference_twoComplexNumbers_differenceValue()
+    {
+      Assert.AreEqual("0+0i", (new ComplexNumber(1, 1) - new ComplexNumber(1, 1)).ToString());
+    }
+  }
+}
---
+++ ComplexNumber/ComplexNumber.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComplexNumber.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComplexNumber.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("426415c5-21d7-44f1-8014-7668b81b2b48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
