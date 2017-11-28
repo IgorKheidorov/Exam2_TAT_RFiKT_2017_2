58d0fdacd0ca6a046cb11f2f9ffc87ffa00f7c2a
ButurlyaAL <stdlibh1421@gmail.com>
2017-11-28 17:18:58 +0200

    added new fuctionality

--- TaskAboutBrackets/Program.cs
+++ TaskAboutBrackets/Program.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TaskAboutBrackets
 {
-    /// <summary>
-    /// Entry point.
-    /// </summary>
-    class Program
+  /// <summary>
+  /// Entry point.
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            StringValidator validator = new StringValidator();
-            Console.WriteLine("Enter an expression :");
-            Console.WriteLine(validator.StringIsValid(Console.ReadLine()));
-        }
+      StringValidator validator = new StringValidator();
+      Console.WriteLine("Enter an expression :");
+      Console.WriteLine(validator.StringIsValid(Console.ReadLine()));
     }
+  }
 }
--- TaskAboutBrackets/StringValidator.cs
+++ TaskAboutBrackets/StringValidator.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TaskAboutBrackets
 {
+  /// <summary>
+  /// Validates string with algebraic expressions.
+  /// </summary>
+  public class StringValidator
+  {
+    private List<char> openingBrackets = new List<char>() {'(', '{', '['};
+    private List<char> closingBrackets = new List<char>() { ')', '}', ']'};
+
+    private char ReverseBracket(char bracket)
+    {
+      if (openingBrackets.Contains(bracket))
+      {
+        return closingBrackets[openingBrackets.IndexOf(bracket)];
+      }
+      else
+      {
+        return openingBrackets[closingBrackets.IndexOf(bracket)];
+      }
+    }
+
     /// <summary>
     /// Validates string with algebraic expressions.
     /// </summary>
-    public class StringValidator
+    /// <param name="str">String to analyze.</param>
+    /// <returns>True if brackets in string with valid alignment.False otherwise.</returns>
+    public bool StringIsValid(string str)
     {
-        private List<char> openingBrackets = new List<char>() {'(', '{', '['};
-        private List<char> closingBrackets = new List<char>() { ')', '}', ']'};
-
-        private char ReverseBracket(char bracket)
+      Stack<char> brackets = new Stack<char>();        
+      for (int i = 0; i < str.Length; i++)
+      {
+        if (openingBrackets.Contains(str[i]))
         {
-            if (openingBrackets.Contains(bracket))
-            {
-                return closingBrackets[openingBrackets.IndexOf(bracket)];
-            }
-            else
-            {
-                return openingBrackets[closingBrackets.IndexOf(bracket)];
-            }
+          brackets.Push(str[i]);
         }
-        /// <summary>
-        /// Validates string with algebraic expressions.
-        /// </summary>
-        /// <param name="str">String to analyze.</param>
-        /// <returns>True if brackets in string with valid alignment.False otherwise.</returns>
-        public bool StringIsValid(string str)
+        if (brackets.Count > 0)
         {
-            Stack<char> brackets = new Stack<char>();
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (openingBrackets.Contains(str[i]))
-                {
-                    brackets.Push(str[i]);
-                }
-                if (brackets.Count > 0)
-                {
-                    if (closingBrackets.Contains(str[i]) && str[i] == ReverseBracket(brackets.Peek()))
-                    {
-                        brackets.Pop();
-                    }
-                }  
-              
-            }
-            return brackets.Count == 0;           
-        }       
-    }
+          if (closingBrackets.Contains(str[i]) && str[i] == ReverseBracket(brackets.Peek()))
+          {                       
+            brackets.Pop();
+          }           
+        }
+        else if (closingBrackets.Contains(str[i]))
+        {
+          return false;
+        }
+      }
+      return brackets.Count == 0;           
+    }       
+  }
 }
