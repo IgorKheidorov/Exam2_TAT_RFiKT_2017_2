bedf219be5d4e387b92f6e92448efae5b4f13fd7
ButurlyaAL <stdlibh1421@gmail.com>
2017-11-29 20:57:16 +0200

    tests

---
+++ TaskAboutLists.Tests/MyLinkedListTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskAboutLists.Tests
+{
+    [TestClass]
+    public class MyLinkedListTests
+    {
+        [TestMethod]
+        public void TestContains()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            Assert.IsTrue( list.Contains("firstValue"));
+            Assert.IsFalse(list.Contains("something"));
+        }
+
+        [TestMethod]
+        public void TestAddOnCount()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            Assert.AreEqual(1, list.count);                    
+        }
+
+        [TestMethod]
+        public void TestGetElelement()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            Assert.AreEqual("firstValue", list.GetElement("firstValue").value);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            Assert.AreEqual("firstValue", list.GetFirst());
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            list.Add("secondValue");
+            list.Remove("firstValue");
+            Assert.IsFalse(list.Contains("secondValue"));
+        }
+
+        [TestMethod]
+        public void TestRemoveFirst()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            list.Add("secondValue");
+            list.RemoveFirst();
+            Assert.IsFalse(list.Contains("firstValue"));
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("firstValue");
+            list.Add("secondValue");
+            list.Clear();
+            Assert.IsFalse(list.Contains("firstValue"));
+            Assert.IsFalse(list.Contains("secondValue"));
+        }
+
+
+
+    }
+}
---
+++ TaskAboutLists.Tests/UnidirectionalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskAboutLists.Tests
+{
+    [TestClass]
+    public class UnidirectionalTests
+    {
+
+        [TestMethod]
+        public void TestAddOnCount()
+        {
+            UnidirectionalList<string> list = new UnidirectionalList<string>();
+            list.Add("firstValue");
+            Assert.AreEqual(1, list.count);
+        }
+    }
+    }
+
