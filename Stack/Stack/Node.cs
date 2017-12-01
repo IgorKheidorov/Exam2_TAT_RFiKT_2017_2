6e395bf10b8cd7c90c8c874e491049867d10acfa
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-12-01 20:53:38 +0300

    Added comment to Node

--- Stack/Stack/Node.cs
+++ Stack/Stack/Node.cs
@@ -2,6 +2,10 @@
 
 namespace Stack
 {
+  /// <summary>
+  /// Node which can be kept in Stack 
+  /// </summary>
+  /// <typeparam name="T">Type of data saved in the list. It can be of any type</typeparam>
   public class Node<T>
   {
     public T Element { get; set; }
