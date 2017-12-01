9b8723045979ec701f1c861f85c35c6974d7c14f
ButurlyaAL <stdlibh1421@gmail.com>
2017-12-01 19:54:45 +0200

    stacktests

---
+++ TaskAboutStack.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TaskAboutStack.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TaskAboutStack.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("f21c4d97-5bdc-425c-8544-821584515706")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskAboutStack.Tests/StackTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskAboutStack.Tests
+{
+  [TestClass]
+  public class StackTests
+  {
+    [TestMethod]
+    public void TestPushOnCount()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      Assert.IsTrue(stack.count == 1);
+    }
+
+    [TestMethod]
+    public void TestPeek()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      Assert.AreEqual("1", stack.Peek());
+      Assert.IsTrue(stack.count == 1);
+    }
+
+    [TestMethod]
+    public void TestPushWithPeek()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      stack.Push("2");
+      stack.Push("3");
+      stack.Push("4");
+      Assert.AreEqual("4", stack.Peek());
+    }
+
+    [TestMethod]
+    public void TestPopWithOneElement()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      Assert.AreEqual("1", stack.Pop());
+      Assert.IsTrue(stack.count == 0);
+    }
+
+    [TestMethod]
+    public void TestPopWithTwoElements()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      stack.Push("2");
+      Assert.AreEqual("2", stack.Pop());
+      Assert.AreEqual("1", stack.Peek());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestPopOnEmptyStack()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Pop();
+     
+    }
+
+    [TestMethod]
+    public void TestClearWithCount()
+    {
+      MyStack<string> stack = new MyStack<string>();
+      stack.Push("1");
+      stack.Push("2");
+      stack.Push("3");
+      stack.Push("4");
+      stack.Push("5");
+      stack.Clear();
+      Assert.IsTrue(stack.count == 0);
+    }
+
+    [TestMethod]
+    public void TestIsEmptyOnEmptyStack()
+    {
+      MyStack<string> stack = new MyStack<string>();  
+      Assert.IsTrue(stack.IsEmpty());
+    }
+  }
+}
