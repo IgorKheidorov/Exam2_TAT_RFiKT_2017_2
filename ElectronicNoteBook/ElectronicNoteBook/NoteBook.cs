e4b1a8db3a97c69fd8fab5e803b8ac31e3fdbe68
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-12-02 20:46:07 +0300

    comments to NoteBook before epic fail

--- ElectronicNoteBook/ElectronicNoteBook/NoteBook.cs
+++ ElectronicNoteBook/ElectronicNoteBook/NoteBook.cs
@@ -34,6 +34,11 @@ namespace ElectronicNoteBook
       Notations = new List<Note>();
     }
 
+    /// <summary>
+    /// Checks if object is equal to this notebook
+    /// </summary>
+    /// <param name="obj">object to compare with</param>
+    /// <returns>True if object equal to this notebook and false if it is not</returns>
     public override bool Equals(object obj)
     {
       if (obj == null || GetType() != obj.GetType())
@@ -44,11 +49,19 @@ namespace ElectronicNoteBook
       return Notations.Equals(noteBook.Notations) && DateOfCreation.Equals(noteBook.DateOfCreation) && Id == noteBook.Id;
     }
 
+    /// <summary>
+    /// Gets Hash Code
+    /// </summary>
+    /// <returns>seconds of creation multiplied by inutes of creation and id number of notebook</returns>
     public override int GetHashCode()
     {
       return DateOfCreation.Second * DateOfCreation.Minute * Id;
     }
 
+    /// <summary>
+    /// Represents NoteBook as string
+    /// </summary>
+    /// <returns>String representation of NoteBook</returns>
     public override string ToString()
     {
       StringBuilder output = new StringBuilder("NoteBook id# " + Id + "\n Date and Time of creation: " + DateOfCreation + "\n");
