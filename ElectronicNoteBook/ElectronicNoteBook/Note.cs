a7ab773b1612fae431bd0aa7ca62d15c78470906
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-12-02 20:43:18 +0300

    comments to Note before epic fail

--- ElectronicNoteBook/ElectronicNoteBook/Note.cs
+++ ElectronicNoteBook/ElectronicNoteBook/Note.cs
@@ -31,6 +31,11 @@ namespace ElectronicNoteBook
       Notation = data;
     }
 
+    /// <summary>
+    /// Checks if object is equal to this note
+    /// </summary>
+    /// <param name="obj">object to compare with</param>
+    /// <returns>True if object equal to this note and false if it is not</returns>
     public override bool Equals(object obj)
     {
       if (obj == null || GetType() != obj.GetType())
@@ -41,11 +46,19 @@ namespace ElectronicNoteBook
       return (DateOfCreation.Equals(note.DateOfCreation)) && (Notation.Equals(note.Notation));
     }
 
+    /// <summary>
+    /// Gets Hash Code
+    /// </summary>
+    /// <returns>minutes of creation multiplied by seconds and milliseconds of creation divided by 1000</returns>
     public override int GetHashCode()
     {
       return DateOfCreation.Minute * DateOfCreation.Second * DateOfCreation.Millisecond / 1000;
     }
 
+    /// <summary>
+    /// Represents Note as string
+    /// </summary>
+    /// <returns>String representation of Note</returns>
     public override string ToString()
     {
       StringBuilder output = new StringBuilder("Date and time of creation: " + DateOfCreation.ToString() + "\n");
